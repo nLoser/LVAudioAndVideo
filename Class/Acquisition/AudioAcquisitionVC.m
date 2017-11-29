e932c226a21ea2a99681355233a45aa02734f848
LV <954777574@qq.com>
2017-11-29 22:44:46 +0800

    audio acquisition

--- Class/Acquisition/AudioAcquisitionVC.m
+++ Class/Acquisition/AudioAcquisitionVC.m
@@ -31,7 +31,7 @@
 - (void)configure {
     OSStatus status;
     
-    //创建AuidoUnit
+    //[1]创建AuidoUnit
     AudioComponentDescription compoentDesc;
     compoentDesc.componentType      = kAudioUnitType_Output;
     compoentDesc.componentSubType   = kAudioUnitSubType_RemoteIO;
@@ -42,7 +42,7 @@
     AudioComponent inputComponent = AudioComponentFindNext(NULL, &compoentDesc);
     status = AudioComponentInstanceNew(inputComponent, &audioUnit);
     
-    //为录制播放开启IO
+    //[2]为录制播放开启IO
     UInt32 flag = 1;
     status = AudioUnitSetProperty(audioUnit,
                                   kAudioOutputUnitProperty_EnableIO,
@@ -57,7 +57,7 @@
                                   &flag,
                                   sizeof(flag));
     
-    //描述音频格式
+    //[3]描述音频格式
     audioFormat.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
     audioFormat.mSampleRate = 44100; ///< 标准44.1KHZ
     audioFormat.mFormatID = kAudioFormatLinearPCM; //liner pulse-code modulation
@@ -66,11 +66,74 @@
     audioFormat.mBitsPerChannel = 16; //采样点16位
     audioFormat.mBytesPerPacket = 2; //数据包的字节总数
     audioFormat.mBytesPerFrame = 2; //每帧子节数
+    status = AudioUnitSetProperty(audioUnit,
+                                  kAudioUnitProperty_StreamFormat,
+                                  kAudioUnitScope_Input,
+                                  kInputBus,
+                                  &audioFormat,
+                                  sizeof(audioFormat));
+    status = AudioUnitSetProperty(audioUnit,
+                                  kAudioUnitProperty_StreamFormat,
+                                  kAudioUnitScope_Output,
+                                  kOutputBus,
+                                  &audioFormat,
+                                  sizeof(audioFormat));
     
+    //[4]为录制播放设置回调函数
+    AURenderCallbackStruct inputCallbackStruct;
+    inputCallbackStruct.inputProcRefCon = (__bridge void * _Nullable)(self);
+    inputCallbackStruct.inputProc = inputCallback;
+    status = AudioUnitSetProperty(audioUnit,
+                                  kAudioOutputUnitProperty_SetInputCallback,
+                                  kAudioUnitScope_Global,
+                                  kInputBus,
+                                  &inputCallbackStruct,
+                                  sizeof(inputCallbackStruct));
+    AURenderCallbackStruct outputCallbackkStruct;
+    outputCallbackkStruct.inputProcRefCon = (__bridge void * _Nullable)(self);
+    outputCallbackkStruct.inputProc = outputCallback;
+    status = AudioUnitSetProperty(audioUnit,
+                                  kAudioUnitProperty_SetRenderCallback,
+                                  kAudioUnitScope_Global,
+                                  kOutputBus,
+                                  &outputCallbackkStruct,
+                                  sizeof(outputCallbackkStruct));
+    
+    //[5]使用自己分配的缓冲区，关闭系统提供的缓冲区
+    flag = 0;
+    status = AudioUnitSetProperty(audioUnit,
+                                  kAudioUnitProperty_ShouldAllocateBuffer,
+                                  kAudioUnitScope_Output,
+                                  kInputBus,
+                                  &flag,
+                                  sizeof(flag));
+    
+    //[6]实例化Audio Unit
+    status = AudioUnitInitialize(audioUnit);
 }
 
 #pragma mark - Private Method
 
+OSStatus inputCallback(void *                            inRefCon,
+                       AudioUnitRenderActionFlags *    ioActionFlags,
+                       const AudioTimeStamp *            inTimeStamp,
+                       UInt32                            inBusNumber,
+                       UInt32                            inNumberFrames,
+                       AudioBufferList * __nullable    ioData)
+{
+    
+    return noErr;
+}
 
+OSStatus outputCallback(void *                            inRefCon,
+                        AudioUnitRenderActionFlags *    ioActionFlags,
+                        const AudioTimeStamp *            inTimeStamp,
+                        UInt32                            inBusNumber,
+                        UInt32                            inNumberFrames,
+                        AudioBufferList * __nullable    ioData)
+{
+    
+    return noErr;
+}
 
 @end
